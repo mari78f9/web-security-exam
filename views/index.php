2ba2d31265737f58bc1ccf4b872c0ca92578e095
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-06-08 19:26:47 +0200

    style sheets

--- views/index.php
+++ views/index.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../_.php';
 
     <section class="index-header">
         <div class="index-icon">
-            <!-- <img> -->
+            <img href="../icons/lock.png" alt="Lock">
         </div>
         <h1> Welcome to the SUCK Database </h1>
         <h2> Choose your user-condition </h2>
@@ -26,7 +26,7 @@ require_once __DIR__ . '/../_.php';
             </a>
 
             <a href="/views/login.php">
-                <button> Team </button>
+                <button> Team-member </button>
             </a>
 
         </div>
