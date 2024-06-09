c89c0334dd95cb98ea3d5aba46833d6edc0330a9
Emma Rodam <emmarodam@gmail.com>
2024-06-09 09:22:17 +0200

    Comments seeders + lieutenant

--- seeders/seed_cases.php
+++ seeders/seed_cases.php
@@ -1,13 +1,20 @@
 <?php
+// Include the required file for database and other configurations
 require_once __DIR__.'/../_.php';
+// Include the Faker library for generating fake data
 require_once __DIR__ . '/Faker/autoload.php';
+// Create a Faker instance to generate fake data
 $faker = Faker\Factory::create();
 
 try {
+
+    // Connecting to database with _db() function from database.php
     $db = _db();
+    // Prepare a SQL statement to drop the cases table if it exists
     $q = $db->prepare('DROP TABLE IF EXISTS cases');
     $q->execute();
 
+    // Prepare a SQL statement to create the users table with the specified schema
     $q = $db->prepare('
         CREATE TABLE cases (
             case_id           VARCHAR(10),
@@ -31,8 +38,11 @@ try {
      // Prepare the statement
      $q = $db->prepare($sql);
 
+    // Loop for generating 20 fake cases
     for ($i = 0; $i < 20; $i++) {
-        $case_id = bin2hex(random_bytes(5));
+        $case_id = bin2hex(random_bytes(5)); // Random case ID wit 10 characters
+        
+        // Picking a random description from the given options
         $case_description = $faker->randomElement(['Theft of a Car',
         'Assault with a Deadly Weapon',
         'Burglary of a Residence',
@@ -43,13 +53,22 @@ try {
         'Robbery of a Bank',
         'Arson of a Building',
         'Kidnapping of a Child']);
-        $case_tip = '';
-        $case_suspect = str_replace("'", "''", $faker->firstName . ' ' . $faker->lastName);        $case_type = $faker->randomElement(['Theft', 'Assault', 'Burglary', 'Vandalism', 'Fraud']);
+        
+        $case_tip = ''; // Empty string for case tips
+        $case_suspect = str_replace("'", "''", $faker->firstName . ' ' . $faker->lastName);        
+        
+        // Picking a random case type from the given options
+        $case_type = $faker->randomElement(['Theft', 
+        'Assault', 
+        'Burglary', 
+        'Vandalism', 
+        'Fraud']);
+        
         $case_location = str_replace("'", "''", $faker->address);
-        $case_solved = rand(0, 1);
+        $case_solved = rand(0, 1); // Randomly deciding if the case is solved or not
         $case_created_at = time();
         $case_updated_at = 0;
-        $case_is_public = rand(0,1);
+        $case_is_public = rand(0,1); // Randomly deciding if the case is public or private
 
         // Bind parameters and execute the prepared statement
         $q->bindParam(':case_id', $case_id);
@@ -66,8 +85,8 @@ try {
         $q->execute();
     }
 
-    echo "+ cases" . PHP_EOL;
+    echo "+ cases" . PHP_EOL; // Success message if the seeding is completed
 } catch (Exception $e) {
-    echo $e->getMessage(); 
+    echo $e->getMessage(); // Error message if not
 }
 ?>
