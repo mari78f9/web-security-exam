3b63ca9ce8cf2c4aefe4d3886437af60858d27ac
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-06-15 17:59:34 +0200

    changes

--- views/create-case.php
+++ views/create-case.php
@@ -1,27 +1,28 @@
 <div class="display-new-case">
-        <div class="create-new-case">
+    <div class="create-new-case">
 
-            <div class="cancel-case">
-                <button onclick="hideNewCrime()"> X </button>
-            </div>
-
-            <h1> New Case: </h1>
-
-            <form id="create-case-form" onsubmit="makeCase(); return false">
-                <label for="case_description">Description:</label>
-                <textarea id="case_description" name="case_description" required></textarea>
-
-                <label for="case_suspect">Suspect:</label>
-                <input type="text" id="case_suspect" name="case_suspect" required>
-
-                <label for="case_type">Type:</label>
-                <input type="text" id="case_type" name="case_type" required>
-
-                <label for="case_location">Location:</label>
-                <input type="text" id="case_location" name="case_location" required>
-
-                <button type="submit">Create Case</button>
-            </form>
-
+        <div class="cancel-case">
+            <button onclick="hideNewCrime()"> X </button>
         </div>
+
+        <h1> New Case: </h1>
+
+        <form id="create-case-form" onsubmit="makeCase(); return false">
+            <label for="case_description">Description:</label>
+            <textarea id="case_description" name="case_description" required></textarea>
+
+            <label for="case_suspect">Suspect:</label>
+            <input type="text" id="case_suspect" name="case_suspect" required>
+
+            <label for="case_type">Type:</label>
+            <input type="text" id="case_type" name="case_type" required>
+
+            <label for="case_location">Location:</label>
+            <input type="text" id="case_location" name="case_location" required>
+
+            <button type="submit">Create Case</button>
+        </form>
+
     </div>
+    
+</div>
--- views/file-upload.php
+++ views/file-upload.php
@@ -1,37 +1,37 @@
 <div class="display-file-upload" id="displayFileUpload">
-            <h2>File Upload</h2>
-            
-            <form id="file-upload" method="post" enctype="multipart/form-data">
-                <label for="file_name">File Name:</label>
-                <input type="text" id="file_name" name="file_name">
-                <label for="case_id">Case ID:</label>
-                <input type="text" id="case_id" name="case_id">
-                <input type="file" name="file">
-                <input type="submit" value="Upload">
-            </form>
-        </div>
+    <h2>File Upload</h2>
+    
+    <form id="file-upload" method="post" enctype="multipart/form-data">
+        <label for="file_name">File Name:</label>
+        <input type="text" id="file_name" name="file_name">
+        <label for="case_id">Case ID:</label>
+        <input type="text" id="case_id" name="case_id">
+        <input type="file" name="file">
+        <input type="submit" value="Upload">
+    </form>
+</div>
 
-        <script>
-            document.getElementById('file-upload').addEventListener('submit', function(event) {
-                event.preventDefault(); // Prevent the form from submitting the traditional way
-                var formData = new FormData(this);
+<script>
+    document.getElementById('file-upload').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from submitting the traditional way
+        var formData = new FormData(this);
 
-                fetch('../api/api-upload-files.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                    alert(data.info);
-                    if (data.info === 'File uploaded successfully.') {
-                        location.reload(); // Reload the page to show updated content after upload
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    alert('An error occurred while uploading the file.');
-                });
-            });
-        </script>
+        fetch('../api/api-upload-files.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.info);
+            if (data.info === 'File uploaded successfully.') {
+                location.reload(); // Reload the page to show updated content after upload
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred while uploading the file.');
+        });
+    });
+</script>
 
 
