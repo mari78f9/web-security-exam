cf3112246ce729545b202eea18ba1e31cb6101e5
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-06-17 17:25:38 +0200

    create crime changes

--- views/create-case.php
+++ views/create-case.php
@@ -6,6 +6,32 @@
         </div>
 
         <h1> Create New Case </h1>
+        
+        <form id="create-case-form" class="new-case-description" onsubmit="makeCase(); return false">
+            
+            <input type="text" id="case_type" name="case_type" placeholder="Type of crime committed" required>
+            
+            <textarea id="case_description" class="case-description" name="case_description" placeholder="Write a description of the case" required></textarea>    
+            
+            <input type="text" id="case_suspect" name="case_suspect" placeholder="Name a suspect" required>
+
+            <input type="text" id="case_location" name="case_location" placeholder="Location of the crime scene" required>
+
+            <button type="submit">Create Case</button>
+        
+        </form>
+
+    </div>
+
+</div>
+
+
+
+
+            <!-- <button onclick="hideNewCrime()"> X </button>
+        </div>
+
+        <h1> Create New Case </h1>
 
         <form" class="new-case-description" id="create-case-form" onsubmit="makeCase(); return false">
         
@@ -22,4 +48,4 @@
 
     </div>
     
-</div>
+</div> -->
